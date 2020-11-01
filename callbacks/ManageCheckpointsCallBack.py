9cc8de63efcbbf76e001453c937a057b524f9a47
Odyssee <otremoulis@gmail.com>
2020-11-01 11:19:36 +0100

    Add timing

--- callbacks/ManageCheckpointsCallBack.py
+++ callbacks/ManageCheckpointsCallBack.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import logging
+import time
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -7,7 +8,10 @@ log = logging.getLogger(__name__)
 class ManageCheckpoints(tf.keras.callbacks.Callback):
     def __init__(self, checkpoint_manager):
         self.checkpoint_manager = checkpoint_manager
+        self.start_time = time.perf_counter()
 
     def on_epoch_end(self, epoch, logs=None):
         super().on_epoch_end(epoch, logs)
         self.checkpoint_manager.save()
+
+        log.info("Epoch {} Execution time: {}".format(epoch, time.perf_counter() - self.start_time))
