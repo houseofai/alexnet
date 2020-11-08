c38c870d2a6d910d16994cad5b2f7137bad4e152
Odyssee <otremoulis@gmail.com>
2020-11-08 21:39:58 +0100

    Add test images augmentation

--- data_augmentation.py
+++ data_augmentation.py
@@ -13,13 +13,34 @@ def transform(image, label):
     return image, label
 
 
-def transform_test(image, label):
-    image = tf.image.resize(image, [227, 227])
+def crop(image, label):
+    image = tf.image.random_crop(image, size=[227, 227, 3])
     return image, label
 
 
-def crop(image, label):
-    image = tf.image.random_crop(image, size=[227, 227, 3])
+def patches(ds):
+    all_patches = None
+    all_labels = None
+    repeat = 4
+    for step, (images, labels) in enumerate(ds):
+        four_patches = tf.image.extract_patches(images,
+                                                sizes=[1, 227, 227, 1],
+                                                strides=[1, 29, 29, 1],
+                                                rates=[1, 1, 1, 1],
+                                                padding='VALID')
+        if all_patches is None:
+            all_patches = four_patches
+            all_labels = np.repeat(labels, repeat)
+        else:
+            all_patches = np.concatenate((all_patches, four_patches), axis=0)
+            all_labels = np.concatenate((all_labels, np.repeat(labels, repeat)), axis=0)
+
+    all_patches = tf.reshape(all_patches, [all_patches.shape[0] * 2 * 2, 227, 227, 3])
+    return tf.data.Dataset.from_tensor_slices((all_patches, all_labels))
+
+
+def center_crop(image, label):
+    image = tf.image.central_crop(image, central_fraction=0.89)
     return image, label
 
 
@@ -36,7 +57,7 @@ def processing(ds_name, batch_size, crop_amount):
     log.info("* Transforming the dataset:")
     log.info("** Resize")
     ds_train = ds_train.map(transform)
-    ds_test = ds_test.map(transform_test)
+    ds_test = ds_test.map(transform)
 
     log.info("** Flipping horizontally")
     ds_train = ds_train.concatenate(ds_train.map(fliph))
@@ -48,8 +69,15 @@ def processing(ds_name, batch_size, crop_amount):
     log.info("** Repeat {} times".format(crop_amount))
     ds_train = ds_train.repeat(crop_amount)
 
-    log.info("** Cropping")
+    log.info("** Cropping Train Set")
     ds_train = ds_train.map(crop)
+
+    log.info("** Patching Test Set")
+    ds_test_patches = patches(ds_test.batch(tf.cast(tf.divide(batch_size, 4), dtype=tf.int64)))
+
+    log.info("** Cropping Test Set")
+    ds_test = ds_test.map(center_crop)
+    ds_test = ds_test.concatenate(ds_test_patches)
 
     ds_file_size = tf.data.experimental.cardinality(ds_train)  # *crop_amount*2
     log.info("* Train Dataset size estimation: {}".format(ds_file_size))
