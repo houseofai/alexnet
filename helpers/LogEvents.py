1ee48aaa3ad4e19653d55626120714fc7032ee61
Odyssee <otremoulis@gmail.com>
2020-11-11 09:44:45 +0100

    Reformat the logs

--- helpers/LogEvents.py
+++ helpers/LogEvents.py
@@ -11,21 +11,20 @@ class LogEvents:
     """
     Class to log events for Tensorboard
     """
+
     def __init__(self, log_dir):
         """
         Initialize the folders and writers to write the events
         :param log_dir: The log dir
         """
         current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        self.train_log_dir = '{}/{}/train'.format(log_dir, current_time)
-        self.test_log_dir = '{}/{}/test'.format(log_dir, current_time)
-        self.train_summary_writer = tf.summary.create_file_writer(self.train_log_dir, name="trainOdy")
-        self.test_summary_writer = tf.summary.create_file_writer(self.test_log_dir, name="testOdy")
+        self.log_dir = '{}/{}/'.format(log_dir, current_time)
+        self.summary_writer = tf.summary.create_file_writer(self.log_dir)
 
-    def log(self, epoch, train_loss, train_accuracy, test_loss, test_accuracy):
+    def log(self, step, train_loss, train_accuracy, test_loss, test_accuracy):
         """
         Log an event
-        :param epoch: the current epoch
+        :param step: the current step
         :param train_loss: The current training loss
         :param train_accuracy: The current training accuracy
         :param test_loss: The current testing loss
@@ -33,23 +32,25 @@ class LogEvents:
         """
         log.info(
             "[epoch:{}] train loss: {:.4} - train accuracy: {:.4} - test loss: {:.4} - test accuracy: {:.4}".format(
-                epoch, train_loss.result(), train_accuracy.result(), test_loss.result(), test_accuracy.result()))
+                step, train_loss, train_accuracy, test_loss, test_accuracy))
 
-        self.log_train(epoch, train_loss, train_accuracy)
-        self.log_test(epoch, test_loss, test_accuracy)
+        self.log_train(step, train_loss, train_accuracy)
+        self.log_test(step, test_loss, test_accuracy)
 
-    def log_train(self, epoch, train_loss, train_accuracy):
-        with self.train_summary_writer.as_default():
-            tf.summary.scalar('loss', train_loss.result(), step=epoch)
-            tf.summary.scalar('accuracy', train_accuracy.result(), step=epoch)
+    def log_train(self, step, loss, accuracy):
+        self.__log(step, 'train_loss', loss)
+        self.__log(step, 'train_accuracy', accuracy)
 
+    def log_test(self, step, loss, accuracy):
+        self.__log(step, 'test_loss', loss)
+        self.__log(step, 'test_accuracy', accuracy)
+
+    def __log(self, step, name, value):
+        with self.summary_writer.as_default():
+            tf.summary.scalar(name, value, step=step)
+
+    def reset(self, train_loss, train_accuracy, test_loss, test_accuracy):
         train_loss.reset_states()
         train_accuracy.reset_states()
-
-    def log_test(self, epoch, test_loss, test_accuracy):
-        with self.test_summary_writer.as_default():
-            tf.summary.scalar('loss', test_loss.result(), step=epoch)
-            tf.summary.scalar('accuracy', test_accuracy.result(), step=epoch)
-
         test_loss.reset_states()
         test_accuracy.reset_states()
